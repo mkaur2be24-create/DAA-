412a914e60a85595066ce9b1be4c44e230c637e0
Mehakdeep kaur <mkaur2_be24@thapar.edu>
2026-01-21 01:27:45 +0530

    Add files via upload

--- ASSIGNMENT1/ques1.cpp
+++ ASSIGNMENT1/ques1.cpp
@@ -1,1 +1,71 @@
+#include <iostream>
+using namespace std;
 
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[10], R[10];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    while (i < n1)
+        arr[k++] = L[i++];
+
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = (l + r) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
+
+int main()
+{
+    int arr1[] = {12, 11, 13, 5, 6, 7};
+    int arr2[] = {38, 27, 43, 3, 9, 82, 10};
+
+    int n1 = 6;
+    int n2 = 7;
+
+    mergeSort(arr1, 0, n1 - 1);
+    mergeSort(arr2, 0, n2 - 1);
+
+    cout << "Sorted arr1: ";
+    printArray(arr1, n1);
+
+    cout << "\nSorted arr2: ";
+    printArray(arr2, n2);
+
+    return 0;
+}
---
+++ ASSIGNMENT1/ques3.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+int max(int a, int b, int c)
+{
+    if (a > b && a > c)
+        return a;
+    else if (b > c)
+        return b;
+    else
+        return c;
+}
+
+int maxCrossingSum(int arr[], int l, int m, int h)
+{
+    int sum = 0;
+    int left_sum = -9999;
+
+    for (int i = m; i >= l; i--)
+    {
+        sum = sum + arr[i];
+        if (sum > left_sum)
+            left_sum = sum;
+    }
+
+    sum = 0;
+    int right_sum = -9999;
+
+    for (int i = m + 1; i <= h; i++)
+    {
+        sum = sum + arr[i];
+        if (sum > right_sum)
+            right_sum = sum;
+    }
+
+    return left_sum + right_sum;
+}
+
+int maxSubArraySum(int arr[], int l, int h)
+{
+    if (l == h)
+        return arr[l];
+
+    int m = (l + h) / 2;
+
+    return max(
+        maxSubArraySum(arr, l, m),
+        maxSubArraySum(arr, m + 1, h),
+        maxCrossingSum(arr, l, m, h));
+}
+
+int main()
+{
+    int arr[] = {-2, -5, 6, -2, -3, 1, 5, -6};
+    int n = 8;
+
+    cout << "Maximum subarray sum is: "
+         << maxSubArraySum(arr, 0, n - 1);
+
+    return 0;
+}
